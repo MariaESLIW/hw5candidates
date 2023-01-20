858bc881a46f998770795438f7f37bb1fae96862
MariaESLIW <121664848+MariaESLIW@users.noreply.github.com>
2023-01-20 11:04:07 +0200

    Refactoring

---
+++ hw5candidate/InputData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+namespace hw5candidate
+{
+	public class InputData
+	{
+		public InputData()
+		{
+		}
+
+        // Create array of candidates (task 1.5)
+        public static Candidate[] CreateCandidates()
+        {
+            Candidate[] candidates = new Candidate[5];
+
+            SubjectScore[] subjectScores0 = {
+                new SubjectScore("Physics", 10),
+                new SubjectScore("Math", 7),
+                new SubjectScore("History", 5)
+            };
+            candidates[0] = new Candidate(new Person("Vasilii", "Vasiliev", new Address("Minsk", "Chapaeva", 5, 100)), subjectScores0);
+
+            SubjectScore[] subjectScores1 =
+                {
+                new SubjectScore("Physics", 9),
+                new SubjectScore("Math", 8),
+                new SubjectScore("History", 6)
+            };
+            candidates[1] = new Candidate(new Person("Petia", "Petrov", new Address("Wroclaw", "SilverTower", 5, 78)), subjectScores1);
+
+            SubjectScore[] subjectScores2 =
+                {
+                new SubjectScore("Physics", 6),
+                new SubjectScore("Math", 8),
+                new SubjectScore("History", 7)
+            };
+            candidates[2] = new Candidate(new Person("Sasha", "Sashov", new Address("Minsk", "Gurskogo", 1, 2)), subjectScores2);
+
+            SubjectScore[] subjectScores3 =
+                {
+                new SubjectScore("Physics", 8),
+                new SubjectScore("Math", 9),
+                new SubjectScore("History", 6)
+            };
+            candidates[3] = new Candidate(new Person("Taras Tarasov", "Petrov", new Address("Lviv", "Kultparkivska", 7, 95)), subjectScores3);
+
+            SubjectScore[] subjectScores4 =
+                {
+                new SubjectScore("Physics", 3),
+                new SubjectScore("Math", 5),
+                new SubjectScore("History", 7)
+            };
+            candidates[4] = new Candidate(new Person("Gogi", "Gogadze", new Address("Batumi", "Batumadze", 105, 79)), subjectScores4);
+
+            return candidates;
+        }
+
+        // task 2.8 create array of university employees
+        public static UniversityEmployee[] CreateUniversityEmployees()
+        {
+            UniversityEmployee universityEmployee1 = new Teacher(
+                "301011975B072PB1",
+                new Person("Vasilii", "Vasiliev", new Address("Minsk", "Chapaeva", 5, 100)),
+                new Course("Physics", "About everything"));
+
+            UniversityEmployee universityEmployee2 = new Teacher(
+                "301011965B068PB3",
+                new Person("Petia", "Petrov", new Address("Wroclaw", "SilverTower", 5, 78)),
+                new Course("ComputerScience", "Java"));
+
+            UniversityEmployee universityEmployee3 = new SupportStaff(
+                "301011980B072PB7",
+                new Person("Sasha", "Sashov", new Address("Minsk", "Gurskogo", 1, 2)), "driver");
+
+            UniversityEmployee universityEmployee4 = new SupportStaff(
+                "301011990B072PB5",
+                new Person("Taras", "Tarasov", new Address("Lviv", "Kultparkivska", 7, 95)), "security");
+
+            UniversityEmployee universityEmployee5 = new SupportStaff(
+                "401011960B072PB9",
+                new Person("Gogi", "Gogadze", new Address("Batumi", "Batumadze", 105, 79)), "dean");
+
+            UniversityEmployee[] employees =
+                {
+                universityEmployee1,
+                universityEmployee2,
+                universityEmployee3,
+                universityEmployee4,
+                universityEmployee5
+            };
+
+            return employees;
+        }
+
+        // task 3.3 - create university
+        public static University CreateUniversity()
+        {
+            Room[] rooms1 = { Room.Auxiliary, Room.Laboratory, Room.Laboratory };
+            Room[] rooms2 = { Room.Lecture, Room.Lecture, Room.Seminar };
+
+            Building[] buildings = { new Building(rooms1), new Building(rooms2) };
+
+            UniversityEmployee rector = new DegreeTeacher(
+                "301015577B072PB1",
+                new Person("Vladislav", "Vlasov", new Address("Minsk", "Skaryny", 75, 34)),
+                new Course("Math", "Linear Algebra"),
+                "PhD", "Professor");
+
+            University university = new University(
+                CreateUniversityEmployees(),
+                rector,
+                buildings,
+                new Address("Minsk", "Skaryny", 15, 3));
+
+            return university;
+        }
+
+    
+
+        
+    }
+}
+
--- hw5candidate/Program.cs
+++ hw5candidate/Program.cs
@@ -4,16 +4,42 @@ internal class Program
     private static void Main(string[] args)
     {
         // Create array of candidates (task 0.5)
-        Candidate[] candidates = createCandidates();
+        Candidate[] candidates = InputData.CreateCandidates();
 
+        // Print streets (task 1.6)
+        PrintStreets(candidates);
+
+        // Print maxSubjectScore (task 1.7)
+        PrintMaxSubjectScore(candidates);
+
+        // task 2.8 create array of university employees
+        UniversityEmployee[] employees = InputData.CreateUniversityEmployees();
+
+        // task 2.9 - all employees with duties
+        PrintEmployeesDuties(employees);
+
+        // task 2.10 - only teachers with courses
+        PrintTeaches(employees);
+
+        // task 3.3 - create and initialize university
+        University PIG = InputData.CreateUniversity();
+    }
+
+    // Print streets (task 1.6)
+    public static void PrintStreets(Candidate[] candidates)
+    {
         // Print streets (task 1.6)
         for (int i = 0; i < candidates.Length; i++)
         {
             string street = candidates[i].Person.Address.Street;
             Console.WriteLine(street);
         }
+    }
 
-        // Print maxSubjectScore (task 1.7)
+    // Print maxSubjectScore (task 1.7)
+    public static void PrintMaxSubjectScore(Candidate[] candidates)
+    {
+        
         int maxScore = 0;
         string subject = "Math";  // subject for which we try to find maxScore
         bool isContained = false; // chech if subject contained subjectScores of candidates
@@ -45,18 +71,20 @@ internal class Program
         {
             Console.WriteLine($"\nThere's no such subject as a {subject} in subject scores.");
         }
+    }
 
-        // task 2.8 create array of university employees
-        UniversityEmployee[] employees = createUniversityEmployees();
-
-        // task 2.9 - all employees with duties
+    // task 2.9 - all employees with duties
+    public static void PrintEmployeesDuties(UniversityEmployee[] employees)
+    {
         for (int i = 0; i < employees.Length; i++)
         {
             Console.WriteLine($"{employees[i].Person.getName()}: {employees[i].getOfficialDuties()}");
         }
+    }
 
-
-        // task 2.10 - only teachers with courses
+    // task 2.10 - only teachers with courses
+    public static void PrintTeaches(UniversityEmployee[] employees)
+    {
         for (int i = 0; i < employees.Length; i++)
         {
             if (employees[i] is Teacher)
@@ -64,113 +92,5 @@ internal class Program
                 Console.WriteLine($"{employees[i].Person.getName()}: {employees[i].getOfficialDuties()}");
             }
         }
-
-        University PIG = CreateUniversity();
-    }
-
-    // task 3.3 - create university
-    public static University CreateUniversity()
-    {
-        Room[] rooms1 = { Room.Auxiliary, Room.Laboratory, Room.Laboratory };
-        Room[] rooms2 = { Room.Lecture, Room.Lecture, Room.Seminar };
-
-        Building building1 = new Building(rooms1);
-        Building building2 = new Building(rooms2);
-
-        Building[] buildings = { building1, building2 };
-
-        UniversityEmployee rector = new DegreeTeacher(
-            "301015577B072PB1",
-            new Person("Vladislav", "Vlasov", new Address("Minsk", "Skaryny", 15, 3)),
-            new Course("Math", "Linear Algebra"),
-            "PhD", "Professor");
-
-        University university = new University(
-            createUniversityEmployees(),
-            rector,
-            buildings,
-            new Address("Minsk", "Skaryny", 15, 3));
-
-        return university;
-    }
-
-    public static UniversityEmployee[] createUniversityEmployees()
-    {
-        // task 2.8 create array of university employees
-        UniversityEmployee universityEmployee1 = new Teacher("301011975B072PB1",
-            new Person("Vasilii", "Vasiliev", new Address("Minsk", "Chapaeva", 5, 100)),
-            new Course("Physics", "About everything"));
-
-        UniversityEmployee universityEmployee2 = new Teacher("301011965B068PB3",
-            new Person("Petia", "Petrov", new Address("Wroclaw", "SilverTower", 5, 78)),
-            new Course("ComputerScience", "Java"));
-
-        UniversityEmployee universityEmployee3 = new SupportStaff("301011980B072PB7",
-            new Person("Sasha", "Sashov", new Address("Minsk", "Gurskogo", 1, 2)), "driver");
-
-        UniversityEmployee universityEmployee4 = new SupportStaff("301011990B072PB5",
-            new Person("Taras", "Tarasov", new Address("Lviv", "Kultparkivska", 7, 95)), "security");
-
-        UniversityEmployee universityEmployee5 = new SupportStaff("401011960B072PB9",
-            new Person("Gogi", "Gogadze", new Address("Batumi", "Batumadze", 105, 79)), "rector");
-
-        UniversityEmployee[] employees =
-            {
-            universityEmployee1,
-            universityEmployee2,
-            universityEmployee3,
-            universityEmployee4,
-            universityEmployee5
-        };
-
-        return employees;
-    }
-
-    public static Candidate[] createCandidates()
-    {
-        // Create array of candidates (task 1.5)
-        Candidate[] candidates = new Candidate[5];
-
-        Address address = new Address("Minsk", "Chapaeva", 5, 100);
-        Person person = new Person("Vasilii", "Vasiliev", address);
-        SubjectScore subjectScore1 = new SubjectScore("Physics", 10);
-        SubjectScore subjectScore2 = new SubjectScore("Math", 7);
-        SubjectScore subjectScore3 = new SubjectScore("History", 5);
-        SubjectScore[] subjectScores0 = { subjectScore1, subjectScore2, subjectScore3 };
-        candidates[0] = new Candidate(person, subjectScores0);
-
-        address = new Address("Wroclaw", "SilverTower", 5, 78);
-        person = new Person("Petia", "Petrov", address);
-        subjectScore1 = new SubjectScore("Physics", 9);
-        subjectScore2 = new SubjectScore("Math", 8);
-        subjectScore3 = new SubjectScore("History", 6);
-        SubjectScore[] subjectScores1 = { subjectScore1, subjectScore2, subjectScore3 };
-        candidates[1] = new Candidate(person, subjectScores1);
-
-        address = new Address("Minsk", "Gurskogo", 1, 2);
-        person = new Person("Sasha", "Sashov", address);
-        subjectScore1 = new SubjectScore("Physics", 6);
-        subjectScore2 = new SubjectScore("Math", 8);
-        subjectScore3 = new SubjectScore("History", 7);
-        SubjectScore[] subjectScores2 = { subjectScore1, subjectScore2, subjectScore3 };
-        candidates[2] = new Candidate(person, subjectScores2);
-
-        address = new Address("Lviv", "Kultparkivska", 7, 95);
-        person = new Person("Taras Tarasov", "Petrov", address);
-        subjectScore1 = new SubjectScore("Physics", 8);
-        subjectScore2 = new SubjectScore("Math", 9);
-        subjectScore3 = new SubjectScore("History", 6);
-        SubjectScore[] subjectScores3 = { subjectScore1, subjectScore2, subjectScore3 };
-        candidates[3] = new Candidate(person, subjectScores3);
-
-        address = new Address("Batumi", "Batumadze", 105, 79);
-        person = new Person("Gogi", "Gogadze", address);
-        subjectScore1 = new SubjectScore("Physics", 3);
-        subjectScore2 = new SubjectScore("Math", 5);
-        subjectScore3 = new SubjectScore("History", 7);
-        SubjectScore[] subjectScores4 = { subjectScore1, subjectScore2, subjectScore3 };
-        candidates[4] = new Candidate(person, subjectScores4);
-
-        return candidates;
     }
 }
